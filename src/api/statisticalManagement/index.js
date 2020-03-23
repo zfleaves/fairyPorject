ec99c5b06dc19ed225d16f58dd0f09a2b72a1959
mmzsyh <815989056@qq.com>
2020-03-23 21:20:35 +0800

    0323

---
+++ src/api/statisticalManagement/index.js
@@ -0,0 +1,28 @@
+import request from 'util/ajax';
+
+//获取项目名称
+export function getCompanyProject(){
+    return request({
+        url:'/bizbase/project/company',
+        method:'get',
+        
+    })
+}
+//公司名称
+export function getPermissionOrgs(){
+    return request({
+        url:'/passport/org/permission/orgs',
+        method:'get',
+        
+    })
+}
+//生成报表
+export function getDetailAnalysis(data){
+    return request({
+        url:'/contract/reportanalysis/cost/detail/analysis',
+        method:'post',
+        data
+        
+    })
+}
+
